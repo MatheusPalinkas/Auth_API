2c2d17dd320ce9a4acb1587ff8be7dd4095cd655
MatheusPalinkas <matheuspalinkas31@gmail.com>
2023-02-13 03:24:48 -0300

    Criando context

---
+++ Auth.Infra.Data/Context/AuthContext.cs
@@ -0,0 +1,19 @@
+﻿using Auth.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.Infra.Data.Context
+{
+    public class AuthContext : IdentityDbContext<User>
+    {
+        public AuthContext(DbContextOptions<AuthContext> options)
+            : base(options)
+        {
+        }
+    }
+}
